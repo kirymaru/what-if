2d043ffacdc47d449bca47b4c435baa6872201f6
Franky <kirymarukrak@gmail.com>
2023-01-14 15:32:15 +0100

    primer commit

--- src/modules/daybook/store/diario/state.js
+++ src/modules/daybook/store/diario/state.js
@@ -1,4 +1,25 @@
 export default () => ({
     isLoading: true,
-    entries: [1, 2, 3, 4, 5, 6]
+    entries: [{
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: ' Dolore magna occaecat amet velit ipsum magna sunt eu laborum aliquip deserunt consectetur.Mollit adipisicing commodo consequat ex consequat ipsum anim anim.Lorem ullamco cillum dolor aliquip tempor eu.Mollit ipsum pariatur sint esse deserunt esse magna.Sunt dolore nisi fugiat reprehenderit tempor d',
+            picture: null,
+
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: ' Qui ipsum nisi esse sint incididunt veniam elit sit labore id deserunt pariatur elit. Aliqua officia est nisi eu duis irure duis cillum ut consequat nostrud. In cupidatat ea eiusmod incididunt proident ut incididunt anim culpa dolor mollit. Veniam nulla dolor id adipisicing officia. Officia sint magna ex qui laborum cupidatat pariatur do incididunt non et. Labore est mollit consectetur laboris fugiat ipsum esse do sunt aliqua et aliqua et.',
+            picture: null,
+
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: '  Non ipsum fugiat nulla laborum tempor veniam do voluptate consequat id anim laborum cillum. Culpa qui sunt reprehenderit fugiat incididunt cillum Lorem incididunt et irure fugiat. Tempor cupidatat laborum reprehenderit culpa sint ullamco. Mollit deserunt irure elit ex sint sit laborum Lorem aliquip tempor ipsum.',
+            picture: null,
+
+        }
+    ]
 })
